2429976e47fa0d4859cf036503151690a0dc1c0f
Jared S <jaredjames2020@gmail.com>
2020-02-13 18:59:35 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   
   include ActiveModel::Validations
-  validates_with EmailValidator
+  validates_with TitleValidator
   
   validates :title, presence: true
   validates :content, length: { minimum: 250 }

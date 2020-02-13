96aa8473e6996c1181bf1ba0b64def636461541a
Jared S <jaredjames2020@gmail.com>
2020-02-13 18:46:46 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
-  validates :category { is: 
+  validates :category, inclusion: { in: %w(small medium large),
+    message: "%{value} is not a valid size" }
   
 end

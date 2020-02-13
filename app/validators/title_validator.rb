09c64ec889c704558379bebd728f526563d90946
Jared S <jaredjames2020@gmail.com>
2020-02-13 18:58:34 +0000

    Automatically backed up by Learn

--- app/validators/title_validator.rb
+++ app/validators/title_validator.rb
@@ -1,6 +1,11 @@
 class TitleValidator < ActiveModel::Validator
 
   def validate(record)
+    unless record.email.match?(/flatironschool.com/)
+      record.errors[:name] << "We're only allowed to have people who work for the company in the database!"
+    end
+  end
+    
   end
 
 end
